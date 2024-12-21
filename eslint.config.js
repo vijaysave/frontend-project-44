5752298677b82a487dd54d4a8cb20925bf3023c1
Мукане Виджей <mukane2005@mail.ru>
2024-12-21 18:06:43 +0700

    синтаксис 13

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginVue from 'eslint-plugin-vue';
-import { recommended } from '@typescript-eslint/eslint-plugin';
-import { parser } from '@typescript-eslint/parser';
+import tseslintPlugin from '@typescript-eslint/eslint-plugin';
+import tseslintParser from '@typescript-eslint/parser';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...recommended.configs,
+  ...tseslintPlugin.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser } } },
+  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslintParser } } },
 ];
